7d9343c8081c038f5f8b54c9f326074fb95d7ef6
Agyare <kofiagyare0000@gmail.com>
2021-11-22 18:59:57 +0000

    first routes

---
+++ shop2/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using shop2.Models;
+
+namespace shop2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Random()
+        {
+            var product = new Product();
+            product.name = "Television";
+            product.id = 3;
+
+            return View(product);
+        }
+        public ActionResult Add()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Details()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
